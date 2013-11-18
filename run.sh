a3be3bb3c2420919b951a9291535189dc3230dd9
Justin Santa Barbara <justin@fathomdb.com>
2013-11-18 16:48:43 -0500

    Add models to classpath

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-if [ ! -f target/stanford-corenlp-3.3.0-models.jar ]; then
-curl "http://central.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.3.0/stanford-corenlp-3.3.0-models.jar" -o target/stanford-corenlp-3.3.0-models.jar
+if [ ! -f target/models.jar ]; then
+curl "http://central.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.3.0/stanford-corenlp-3.3.0-models.jar" -o target/models.jar
 fi
 
+CLASSPATH_PREFIX="target/models.jar"
+export CLASSPATH_PREFIX
 sh target/bin/webapp
