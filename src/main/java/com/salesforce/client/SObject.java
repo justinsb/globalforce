f78c2c87e79f2ad93d8d9c67498f18fd77191d5f
Justin Santa Barbara <justin@fathomdb.com>
2013-11-18 17:29:07 -0500

    Don't use toString when getting string value

--- src/main/java/com/salesforce/client/SObject.java
+++ src/main/java/com/salesforce/client/SObject.java
@@ -3,42 +3,57 @@ package com.salesforce.client;
 import java.util.Map;
 import java.util.Set;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.common.collect.Sets;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 
 public class SObject {
 
-	private final JsonObject json;
+    private static final Logger log = LoggerFactory.getLogger(SObject.class);
 
-	public SObject(JsonObject json) {
-		this.json = json;
-	}
+    private final JsonObject json;
 
-	public String getId() {
-		return find("Id");
-	}
+    public SObject(JsonObject json) {
+        this.json = json;
+    }
 
-	public String getName() {
-		return find("Name");
-	}
+    public String getId() {
+        return find("Id");
+    }
 
-	public Set<String> keys() {
-		Set<String> keys = Sets.newHashSet();
-		for (Map.Entry<String, JsonElement> entry : json.entrySet()) {
-			String key = entry.getKey();
-			keys.add(key);
-		}
+    public String getName() {
+        return find("Name");
+    }
 
-		return keys;
-	}
+    public Set<String> keys() {
+        Set<String> keys = Sets.newHashSet();
+        for (Map.Entry<String, JsonElement> entry : json.entrySet()) {
+            String key = entry.getKey();
+            keys.add(key);
+        }
 
-	public String find(String key) {
-		JsonElement jsonElement = json.get(key);
-		if (jsonElement == null) {
-			return null;
-		}
-		return jsonElement.toString();
-	}
+        return keys;
+    }
 
+    public String find(String key) {
+        JsonElement jsonElement = json.get(key);
+        if (jsonElement == null) {
+            return null;
+        }
+        if (jsonElement.isJsonPrimitive()) {
+            JsonPrimitive primitive = (JsonPrimitive) jsonElement;
+            if (primitive.isString()) {
+                return primitive.getAsString();
+            } else {
+                log.warn("Key was found, but was not json string: {}={}", key, jsonElement);
+            }
+        } else {
+            log.warn("Key was found, but was not json primitive: {}={}", key, jsonElement);
+        }
+        return null;
+    }
 }
