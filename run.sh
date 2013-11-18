ca6d858491a173554182cd99327ff1993236eeac
Justin Santa Barbara <justin@fathomdb.com>
2013-11-18 16:54:34 -0500

    Switching directory; having problems with target/

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-if [ ! -f target/models.jar ]; then
-curl "http://central.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.3.0/stanford-corenlp-3.3.0-models.jar" -o target/models.jar
+mkdir -p models
+
+if [ ! -f "models/stanford-corenlp-3.3.0-models.jar" ]; then
+curl "http://central.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.3.0/stanford-corenlp-3.3.0-models.jar" -o target/stanford-corenlp-3.3.0-models.jar
 fi
 
-CLASSPATH_PREFIX="target/models.jar"
+CLASSPATH_PREFIX="models/stanford-corenlp-3.3.0-models.jar"
 export CLASSPATH_PREFIX
+
 sh target/bin/webapp
