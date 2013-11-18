452a6dd2782f889a3164ef5ea20838f7112aa530
Justin Santa Barbara <justin@fathomdb.com>
2013-11-18 17:01:17 -0500

    Try downloading again

--- run.sh
+++ run.sh
@@ -3,8 +3,10 @@
 mkdir -p models
 
 if [ ! -f "models/stanford-corenlp-3.3.0-models.jar" ]; then
-curl "http://central.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.3.0/stanford-corenlp-3.3.0-models.jar" -o target/stanford-corenlp-3.3.0-models.jar
+curl "http://central.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.3.0/stanford-corenlp-3.3.0-models.jar" -o models/stanford-corenlp-3.3.0-models.jar
 fi
+
+ls models/
 
 CLASSPATH_PREFIX="models/stanford-corenlp-3.3.0-models.jar"
 export CLASSPATH_PREFIX
