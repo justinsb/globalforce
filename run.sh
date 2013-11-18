162812ef4e51e539487ed7003964c61c957ebdf8
Justin Santa Barbara <justin@fathomdb.com>
2013-11-18 16:43:39 -0500

    [[ not on heroku :-(

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ ! -f target/stanford-corenlp-3.3.0-models.jar ]]; then
+if [ ! -f target/stanford-corenlp-3.3.0-models.jar ]; then
 curl "http://central.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.3.0/stanford-corenlp-3.3.0-models.jar" -o target/stanford-corenlp-3.3.0-models.jar
 fi
 
