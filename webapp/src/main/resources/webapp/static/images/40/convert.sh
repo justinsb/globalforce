4265b8c110f00efc0b494261a4c1f75fb5d7c239
Justin Santa Barbara <justin@fathomdb.com>
2013-11-20 10:52:43 -0800

    Added 40px images for Apex/Visualforce

---
+++ webapp/src/main/resources/webapp/static/images/40/convert.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+convert ../sentiment_n2.jpg -resize 40x40 sentiment_n2.jpg
+convert ../sentiment_n1.jpg -resize 40x40 sentiment_n1.jpg
+convert ../sentiment_0.jpg -resize 40x40 sentiment_0.jpg
+convert ../sentiment_p1.jpg -resize 40x40 sentiment_p1.jpg
+convert ../sentiment_p2.jpg -resize 40x40 sentiment_p2.jpg
